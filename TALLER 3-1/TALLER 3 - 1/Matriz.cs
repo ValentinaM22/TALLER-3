eaec08aa5844bd84e0828b12686bd4dbea4be163
valentinaM22 <valentinamoralesvelasquez@gmail.com>
2025-03-12 20:33:06 -0500

    punto 1

---
+++ TALLER 3-1/TALLER 3 - 1/Matriz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3
+{
+    public class Matriz
+    {
+        private int[,] datos;
+        private int orden;
+
+        public Matriz(int n)
+        {
+            orden = n;
+            datos = new int[n, n];
+            LlenarMatriz();
+        }
+
+        private void LlenarMatriz()
+        {
+            for (int i = 0; i < orden; i++)
+            {
+                for (int j = 0; j < orden; j++)
+                {
+                    datos[i, j] = i + j;
+                }
+            }
+        }
+
+        public void MostrarMatriz()
+        {
+            Console.WriteLine("\nMatriz completa:");
+            for (int i = 0; i < orden; i++)
+            {
+                for (int j = 0; j < orden; j++)
+                {
+                    Console.Write($"{datos[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void MostrarTriangularInferior()
+        {
+            Console.WriteLine("\nTriangular inferior:");
+            for (int i = 0; i < orden; i++)
+            {
+                for (int j = 0; j < orden; j++)
+                {
+                    if (j <= i)
+                        Console.Write($"{datos[i, j],3} ");
+                    else
+                        Console.Write("    ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TALLER 3-1/TALLER 3 - 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Taller3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ingrese orden de la matriz: ");
+            int n = int.Parse(Console.ReadLine() ?? "0");
+
+            Matriz matriz = new Matriz(n);
+
+            matriz.MostrarMatriz();
+            matriz.MostrarTriangularInferior();
+        }
+    }
+}
