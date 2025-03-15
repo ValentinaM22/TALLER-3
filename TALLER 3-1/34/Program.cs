fbd378d57517f3f921bffeeb93d0615ab270b3b0
valentinaM22 <valentinamoralesvelasquez@gmail.com>
2025-03-14 21:48:30 -0500

    Terminado

---
+++ TALLER 3-1/34/Program.cs
@@ -0,0 +1,21 @@
+﻿using _34;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el orden de la matriz (impar): ");
+        int n = int.Parse(Console.ReadLine());
+
+        try
+        {
+            RelojDeArena reloj = new RelojDeArena(n);
+            reloj.MostrarMatrizCompleta();
+            reloj.MostrarRelojDeArena();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
---
+++ TALLER 3-1/34/RelojDeArena.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34
+{
+    class RelojDeArena
+    {
+        private int[,] matriz;
+        private int n;
+
+        public RelojDeArena(int tamaño)
+        {
+            if (tamaño % 2 == 0)
+            {
+                throw new ArgumentException("El tamaño debe ser un número impar.");
+            }
+
+            n = tamaño;
+            matriz = new int[n, n];
+            LlenarMatriz();
+        }
+
+        private void LlenarMatriz()
+        {
+            int valor = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = valor++;
+                }
+            }
+        }
+
+        public void MostrarMatrizCompleta()
+        {
+            Console.WriteLine("\nMATRIZ COMPLETA:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matriz[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void MostrarRelojDeArena()
+        {
+            Console.WriteLine("\nRELOJ DE ARENA:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // Condición para mostrar el reloj de arena
+                    if (j >= i && j < n - i || j <= i && j >= n - 1 - i)
+                    {
+                        Console.Write($"{matriz[i, j],3} ");
+                    }
+                    else
+                    {
+                        Console.Write("    "); // Espacio en blanco para alinear la forma del reloj
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
